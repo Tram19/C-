a4fd81a5ddd5b90ba78a902e310d621aa18aac7d
Tram19 <87849552+Tram19@users.noreply.github.com>
2021-07-23 16:31:44 +0700

    Update Hocluc.cpp

--- Hocluc.cpp
+++ Hocluc.cpp
@@ -4,14 +4,15 @@
 	float a, b, c;
      printf("Nhap diem tb hk2, diem toan, diem van:");
      scanf("%f %f %f", &a, &b, &c);
-     if(a >= 8 && b >= 8) { printf("Hoc luc gioi"); }
-     else if(a >= 8 && c >= 8) { printf("Hoc luc gioi"); }
+     if(a < 0 || a > 10 || b < 0 || b > 10 || c < 0 || c > 10) { printf("ERROR"); }
+     if((a >= 8 && b >= 8 && a <= 10 && b <= 10) || (a >= 8 && c >= 8 && a <= 10 && c <= 10) ) { printf("Hoc luc gioi"); }
      else if((a >= 6.5 && b >= 6.5) || (a >= 6.5 && c >= 6.5) ) { printf("Hoc luc kha"); }
      else if((a >= 5 && b >= 5) || (a >= 5 && c >= 5)) { printf("Hoc luc TB"); }
-     else if(a < 5) { printf("Hoc luc yeu"); }
+     else if(a < 5 && a>=0 ) { printf("Hoc luc yeu"); }
      return 0;
-     }
-		
+     printf("/ERROR/");
+     return 0;
+     }	
 			
  
 	
