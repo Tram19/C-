7acd7ecbd6354d83cf685ed82f68156f471b9f05
Tram19 <87849552+Tram19@users.noreply.github.com>
2021-07-23 15:08:24 +0700

    Add files via upload

---
+++ so nguyen to.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+  main() {
+	int n;
+	printf("n=");
+	scanf("%d", &n);
+
+	    
+	    
+	
+     for(int i = 2; i < n; i++){
+        if(n % i == 0 ) {
+		printf("%d khong la so nguyen to", n);
+	} else { 
+		printf("%d la so nguyen to", n);
+		} 
+		
+	return 0;
+	
+} }
