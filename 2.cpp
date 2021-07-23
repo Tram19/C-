0ca01f689a637d3e03c1a19ff0b0fe1a4a05bbba
Tram19 <87849552+Tram19@users.noreply.github.com>
2021-07-23 14:05:25 +0700

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+ main() {
+	float a, b;
+	while (a <= 200) {
+	printf("C= ");
+	
+	scanf("%f", &a);
+    
+	
+	if (a >= 0) { 
+	b = a * 1.8 + 32 ; }
+	printf("%fdoF\n", b );
+	}
+	
+return 0;
+}
+
