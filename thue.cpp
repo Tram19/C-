30f815f39302ac37c563444efbad772c60bb7335
Tram19 <87849552+Tram19@users.noreply.github.com>
2021-07-23 17:07:39 +0700

    Update thue.cpp

--- thue.cpp
+++ thue.cpp
@@ -4,23 +4,16 @@
 	float a;
      printf("Doanh so= ");
      scanf("%f", &a);
-     
-     if( a <= 200000000 ); { printf("Thue la %f VND", a * 0.03) ; 
-      return 0;
-      }  if( a <= 500000000 ); { printf("Thue la %f VND", a * 0.05) ;
-      return 0;
-     }   if( a >= 1000000000 ); { printf("Thue la %f VND", a * 0.1) ; 
-        return 0;
-	 }
-     
+
+     if( a <= 200000000 ) { printf("\nThue la %f VND", a * 0.03) ; 
       
-     
-	  
+      }  else if( a <= 500000000 ) { printf("\nThue la %f VND", a * 0.05) ;
+      
+     }   else if( a >= 1000000000 )  { printf("\nThue la %f VND", a * 0.1) ; 
     
-     return 0; 
-     
-     
-     }
+	 }
+        return 0;
+        }
 		
 			
  
