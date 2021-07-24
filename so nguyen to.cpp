61b15fb8e4469d8e89ba14cf6df57a2135d880bf
Tram19 <87849552+Tram19@users.noreply.github.com>
2021-07-24 08:50:43 +0700

    Update so nguyen to.cpp

--- so nguyen to.cpp
+++ so nguyen to.cpp
@@ -1,12 +1,12 @@
 #include<stdio.h>
 
-  main() {
+ int main() {
 	int n;
 	printf("n=");
 	scanf("%d", &n);
         if(n < 2){
         printf("\n%d khong phai so nguyen to", n); }
-        return 0;
+        if(n == 2){ printf("\n2 la so nguyen to")  ; }
      for(int i = 2; i < n; i++){
         if(n % i == 0)  {
 		printf("%d khong la so nguyen to", n);
