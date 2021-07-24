8e2a3aabf090d4b3315d6ee8616a8fd7bd2aa550
Tram19 <87849552+Tram19@users.noreply.github.com>
2021-07-24 14:48:21 +0700

    Update 6-dientich.cpp

--- 6-dientich.cpp
+++ 6-dientich.cpp
@@ -12,13 +12,13 @@
 	scanf("%f", &b) ;
 	if (b <= 0) { printf("kich thuoc khong hop le"); }
 	else {
-	printf("Dien tich cua hinh tron la: %f", b * b * 3.14);
+	printf("Dien tich cua hinh tron la: %0.2f", b * b * 3.14);
 	 }  }
 	else if (a == 2) { printf("Nhap kich thuoc : ");
 	scanf("%f%f", &b, &c);
 	if (b <= 0 || c <= 0 ) { printf("kich thuoc khong hop le"); }
 	else {
-	printf("Dien tich cua hinh chu nhat la: %f", b * c);
+	printf("Dien tich cua hinh chu nhat la: %0.2f", b * c);
 	 }; } 
 	
 	else if (a == 3) { printf("Nhap kich thuoc : ");
@@ -26,7 +26,7 @@
 		if (b <= 0 || c <= 0 || d <= 0 || (b + c <= a) || (a + b <= c) || ( a + c <= b)) { printf("kich thuoc khong hop le"); }
 	else {
 		p= (b + c + d) / 2 ;
-	printf("Dien tich cua hinh tam giac la: %f", sqrt( p * (p -b) * (p - c)* (p - d) ) ); }
+	printf("Dien tich cua hinh tam giac la: %0.2f", sqrt( p * (p -b) * (p - c)* (p - d) ) ); }
 	} 	}
 
 
